c4525427fe81f5a1f3d89b0c40d017b4647fed85
Rodion Sibov <rodionsibov@gmail.com>
2021-11-02 22:52:17 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    userData: {}
   },
   mutations: {
+    setUserData(state, val) {
+      state.userData = val
+    }
+  },
+  getters: {
+    user(state) {
+      return state.userData
+    }
   },
   actions: {
+    login({ commit }) {
+      const url = 'https://randomuser.me/api'
+      axios.get(url).then(res => {
+        const userData = {
+          displayName: res.data.result[0].name.first,
+          email: res.data.result[0].email,
+          photoURL: res.data.result[0].picture.thumbnail,
+          uid: res.data.result[0].login.uuid,
+        }
+        commit('setUserData', userData)
+      }).catch(err => console.log(err))
+    }
   },
   modules: {
   }
