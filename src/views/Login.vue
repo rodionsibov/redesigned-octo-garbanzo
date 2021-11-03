e0f62c00c1e28fd7dbca43e65f0f321e31449fbd
Rodion Sibov <rodionsibov@gmail.com>
2021-11-03 10:06:59 +0200

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="container" style="padding-top: 10%">
+      <div class="row d-flex justify-content-center">
+        <div class="col-5 text-left login-form-container">
+          <div class="d-flex justify-content-center">
+            <img
+              src="https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png"
+              width="150"
+              alt=""
+            />
+          </div>
+          <div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Check me out</label
+              >
+            </div>
+            <button
+              @click="login()"
+              type="submit"
+              class="btn btn-primary btn-block"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "Login",
+  methods: {
+    ...mapActions("account", ["login"]),
+  },
+};
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="padding: 25px">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4" v-for="product in products" :key="product.id">
+          <ProductCard :product="product" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import ProductCard from '@/views/ProductCard.vue';
+
+export default {
+  computed: {
+    ...mapGetters(['products']),
+  },
+  components: { ProductCard },
+  methods: {
+    ...mapActions(['getProducts', 'addCart', 'removeCart']),
+  },
+  mounted() {
+    this.getProducts();
+  },
+};
+</script>
+
