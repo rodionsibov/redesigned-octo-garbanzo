24dcbd76a2d6ab4d0a660b56f867d6125a6966ca
Rodion Sibov <rodionsibov@gmail.com>
2021-11-03 09:11:10 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,25 @@ export default createStore({
     userData: {},
     products: [],
     product: {},
-    cart: []
+    cart: [],
+    uploadingData: {}
   },
   mutations: {
     setUserData(state, val) {
       state.userData = val
     },
-    
+    setProducts(state, val) {
+      state.products = val
+    },
+    setProduct(state, val) {
+      state.product = val
+    },
+    setLoad(state, val) {
+      state.uploadingData = val
+    },
+    setCart(state, val) {
+      state.cart = val
+    }
   },
   getters: {
     user(state) {
@@ -40,6 +52,36 @@ export default createStore({
         }
         commit('setUserData', userData)
       }).catch(err => console.log(err))
+    },
+    getProducts({ commit }) {
+      const url = 'https://my-json-server.typicode.com/Nelzio/ecommerce-fake-json/products'
+      axios.get(url).then(res => {
+        commit('setProducts', res.data)
+      }).catch(err => console.log(err))
+    },
+    productDetails({ commit }, id) {
+      const url = 'https://my-json-server.typicode.com/Nelzio/ecommerce-fake-json/products'
+      axios.get(url, { params: { id: id } }).then(res => {
+        commit('setProduct', res.data[0])
+      }).catch(err => console.log(err))
+    },
+    addCart({ commit, getters }, payload) {
+      const cart = getters.cart
+      cart.push(payload)
+      commit('setCart', cart)
+    },
+    removeCart({ commit, getters }, id) {
+      const cart = []
+      if (id) {
+        for (let index = 0; index < getters.cart.length; index++) {
+          const element = getters.cart[index]
+          if (element.id !== id) {
+            cart.push(element)
+          }
+
+        }
+      }
+      commit('setCart', cart)
     }
   },
   modules: {
